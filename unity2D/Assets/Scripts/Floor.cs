687701aceeecd7b322fc4b091db1e0349fbe1e3e
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2019-11-13 21:41:21 +0800

    認識API

--- unity2D/Assets/Scripts/Floor.cs
+++ unity2D/Assets/Scripts/Floor.cs
@@ -2,7 +2,16 @@
 
 public class Floor : MonoBehaviour
 {
-    [Header("速度"), Range(0.5f, 50f)]
+    
+
+
+    [Header("速度"), Range(0.5f, 10f)]
     public float speed = 1.5f;
 
+    public Transform floor;
+
+    private void Update()
+    {
+        floor.Translate(-speed, 0, 0);
+    }
 }
---
+++ unity2D/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("一般!");
+        Debug.LogWarning("警告!");
+        Debug.LogError("錯誤!");
+    }
+}
