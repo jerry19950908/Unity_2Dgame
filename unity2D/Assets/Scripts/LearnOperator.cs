6760e40eb3642587659f3d26fff4c4e1856ed442
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2019-11-22 21:33:49 +0800

    認識運算式

---
+++ unity2D/Assets/Scripts/LearnOperator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+
+{
+    public int a = 10, b = 3;
+    public int c = 20, d = 7;
+      private void Start()
+    {
+        #region 數學運算子
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b); //餘數 ex: 10/3 = 3.....1  取餘數
+
+        // = 指定符號,右邊先運算
+        a = a + 1;
+        print(a);
+
+        print(a++); //先輸出 a 在遞增  皆等於a+1
+        print(++a); //先遞增再輸出 a   皆等於a+1
+
+        a = a + 10; // 加10的意思
+        a += 10;    // 同上  也可以運用到 += -=  *=  /=  %=
+        print(a);
+        #endregion
+
+        #region 比較運算值(只會傳回布林值)
+        print(c > d);  // true
+        print(c < d);  // false
+        print(c >= d); // true 
+        print(c <= d); // false
+        print(c == d); // 等於的意思  false 
+        print(c != d); // 不等於的意思 true
+        #endregion
+
+        #region 邏輯運算值
+        // 並且 && shift + 7
+        // 只要有一個 false 結果為 false
+        print(true && true);    // t
+        print(true && false);   // f
+        print(false && true);   // f
+        print(false && false);  // f
+        // 或者 || shift + 鎮
+        // 只要有一個 true 結果為 true
+        print(true || true);    // t
+        print(true || false);   // t
+        print(false || true);   // t
+        print(false || false);  // f
+
+
+        // 相反 !
+        // 將布林值改為相反
+        print(!true);     // false
+        print(!false);    // true
+
+        
+        #endregion
+
+
+
+    }
+
+
+
+
+
+}
---
+++ unity2D/Assets/Scripts/Learnif.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class Learnif : MonoBehaviour
+{
+
+    public bool open;
+    public int score;
+    public int combo;
+
+    private void Start()
+    {
+        //判斷式if
+        // if(布林值) {當布林值為 true 才執行}
+        if (true)
+        {
+            print("嗨");
+        }
+        
+
+
+        //當布林值為true 會執行 if{}
+        if (open)
+        {
+            print("開門~~");
+        }
+        //當布林值為 false 會直接 else{}
+        else
+        {
+            print("關門~~");
+        }
+
+
+
+
+        if (score >= 60)
+        {
+            print("及格囉~~");
+        }
+
+        else if(score >= 40)
+        {
+            print("可補考");
+        }
+
+        else
+        {
+            print("你被當了~~");
+        }
+
+        
+        if(combo >= 50 && combo < 100)
+        {
+            print("攻擊x2");
+        }
+        else if(combo >= 100 && combo < 150)
+        {
+            print("攻擊x5");
+        }
+        else if (combo >= 150)
+        {
+            print("攻擊x10");
+        }
+        else
+        {
+            print("攻擊x1");
+        }
+    }
+}
